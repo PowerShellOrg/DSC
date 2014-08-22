9545c6259b27657469349bfd94697750ec51f17e
Dave Wyatt <dlwyatt115@gmail.com>
2014-08-21 23:12:44 -0400

    Updated Pester test code to work with new variable resolution technique.
    $PSCmdlet.GetVariableValue returns the value of the parameter, if the caller happens to be in the same module / sessionstate as the Resolve-DscConfigurationProperty function.  Normally, this is not the case, but because of the way the test script was loading the ps1 file instead of just importing the module, this was breaking the tests.
    
    It shouldn't be necessary to "fix" this behavior in the module itself, since nothing within the module calls Resolve-DscConfigurationProperty, nor should there ever be a need for that.

--- Tooling/DscConfiguration/Resolve-ConfigurationProperty.Tests.ps1
+++ Tooling/DscConfiguration/Resolve-ConfigurationProperty.Tests.ps1
@@ -1,17 +1,5 @@
-$here = Split-Path -Parent $MyInvocation.MyCommand.Path
-$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.ps1", ".psm1")
-if (-not (Test-Path $sut))
-{
-	$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.ps1", ".ps1")
-}
-$pathtosut = join-path $here $sut
-if (-not (Test-Path $pathtosut))
-{
-    Write-Error "Failed to find script to test at $pathtosut"
-}
-
-
-iex ( gc $pathtosut -Raw )
+Remove-Module DscConfiguration -Force -ErrorAction SilentlyContinue
+Import-Module -Name $PSScriptRoot\DscConfiguration.psd1 -Force -ErrorAction Stop
 
 describe 'how Resolve-DscConfigurationProperty responds' {
     
