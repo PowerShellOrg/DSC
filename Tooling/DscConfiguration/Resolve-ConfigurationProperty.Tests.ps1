82750ed5ce950e2698a0a4ae76a528a78c6012af
Dave Wyatt <dlwyatt115@gmail.com>
2014-08-28 21:45:40 -0400

    Reverting some changes (tests only)
    Had another chat with Steve Murawski, and he convinced me of the value of some of the old behavior that I had cut out (being able to override values for individual services at the site / node level.)  While this does reintroduce some complexity, it can be addressed with sufficient documentation.
    
    I've brought back some of the original test code to guide the upcoming changes.

--- Tooling/DscConfiguration/Resolve-ConfigurationProperty.Tests.ps1
+++ Tooling/DscConfiguration/Resolve-ConfigurationProperty.Tests.ps1
@@ -1,7 +1,176 @@
 Remove-Module DscConfiguration -Force -ErrorAction SilentlyContinue
 Import-Module -Name $PSScriptRoot\DscConfiguration.psd1 -Force -ErrorAction Stop
 
-Describe 'Resolve-DscConfigurationProperty' {
+describe 'how Resolve-DscConfigurationProperty responds' {
+    $ConfigurationData = @{
+        AllNodes = @();
+        SiteData = @{ NY = @{ PullServerPath = 'ConfiguredBySite' } };
+        Services = @{};
+        Applications = @{};
+    }
+
+    context 'when a node has an override for a site property' {
+        $Node = @{
+            Name = 'TestBox'
+            Location = 'NY'
+            PullServerPath = 'ConfiguredByNode'
+        }
+
+        $result = Resolve-DscConfigurationProperty -Node $Node -PropertyName 'PullServerPath'
+
+        it "should return the node's override" {
+            $result | should be 'ConfiguredByNode'
+        }
+    }
+
+    context 'when a node does not override the site property' {
+        $Node = @{
+            Name = 'TestBox'
+            Location = 'NY'
+        }
+
+        $result = Resolve-DscConfigurationProperty -Node $Node -PropertyName 'PullServerPath'
+        it "should return the site's default value" {
+            $result | should be 'ConfiguredBySite'
+        }
+    }
+
+    context 'when a specific site does not have the property but the base configuration data does' {
+        $ConfigurationData.SiteData = @{
+            All = @{ PullServerPath = 'ConfiguredByDefault' }
+            NY = @{ PullServerPath = 'ConfiguredBySite' }
+        }
+
+        $Node = @{
+            Name = 'TestBox'
+            Location = 'OR'
+        }
+
+        $result = Resolve-DscConfigurationProperty -Node $Node -PropertyName 'PullServerPath'
+        it "should return the site's default value" {
+            $result | should be 'ConfiguredByDefault'
+        }
+    }
+}
+
+describe 'how Resolve-DscConfigurationProperty (services) responds' {
+    $ConfigurationData = @{AllNodes = @(); SiteData = @{} ; Services = @{}; Applications = @{}}
+
+    $ConfigurationData.Services = @{
+        MyTestService = @{
+            Nodes = @('TestBox')
+            DataSource = 'MyDefaultValue'
+        }
+    }
+
+    context 'when a default value is supplied for a service and node has a property override' {
+
+        $Node = @{
+            Name = 'TestBox'
+            Location = 'NY'
+            Services = @{
+                MyTestService = @{
+                    DataSource = 'MyCustomValue'
+                }
+            }
+        }
+
+        $result = Resolve-DscConfigurationProperty -Node $Node -PropertyName DataSource
+
+        it 'should return the override from the node' {
+            $result | should be 'MyCustomValue'
+
+        }
+    }
+
+    context 'when a site level override is present' {
+        $ConfigurationData.SiteData = @{
+            NY = @{
+                Services = @{
+                    MyTestService = @{
+                        DataSource = 'MySiteValue'
+                    }
+                }
+            }
+        }
+        $Node = @{
+            Name = 'TestBox'
+            Location = 'NY'
+        }
+
+        $result = Resolve-DscConfigurationProperty -Node $Node -PropertyName DataSource
+
+        it 'should return the override from the site' {
+            $result | should be 'MySiteValue'
+        }
+    }
+
+    context 'when no node or site level override is present' {
+        $ConfigurationData.Services = @{
+            MyTestService = @{
+                Nodes = @('TestBox')
+                DataSource = 'MyDefaultValue'
+            }
+        }
+        $ConfigurationData.SiteData = @{
+            All = @{ DataSource = 'NotMyDefaultValue'}
+            NY = @{
+                Services = @{
+                    MyTestService = @{}
+                }
+            }
+        }
+        $Node = @{
+            Name = 'TestBox'
+            Location = 'NY'
+        }
+
+        $result = Resolve-DscConfigurationProperty -Node $Node -PropertyName DataSource
+
+        it 'should return the default value from the service' {
+            $result | should be 'MyDefaultValue'
+
+        }
+    }
+
+    context 'when no service default is specified' {
+
+        $Node = @{
+            Name = 'TestBox'
+            Location = 'NY'
+            MissingFromFirstServiceConfig = 'FromNodeWithoutService'
+        }
+
+        $result = Resolve-DscConfigurationProperty -Node $Node -PropertyName MissingFromFirstServiceConfig
+        it 'should fall back to checking for the parameter outside of Services' {
+            $result | should be 'FromNodeWithoutService'
+        }
+    }
+
+    context 'When multiple services exist, but only one contains the desired property' {
+        $ConfigurationData.Services = @{
+            MyTestService = @{
+                Nodes = @('TestBox')
+            }
+            MySecondTestService = @{
+                Nodes = @('TestBox')
+                MissingFromFirstServiceConfig = 'FromSecondServiceConfig'
+            }
+        }
+        $Node = @{
+            Name = 'TestBox'
+            Location = 'NY'
+        }
+
+        $result = Resolve-DscConfigurationProperty -Node $Node -PropertyName MissingFromFirstServiceConfig
+
+        it 'should retrieve the parameter from the second service' {
+            $result | should be 'FromSecondServiceConfig'
+        }
+    }
+}
+
+Describe 'Resolving multiple properties' {
     $ConfigurationData = @{
         AllNodes = @();
 
@@ -73,32 +242,6 @@ Describe 'Resolve-DscConfigurationProperty' {
         }
     }
 
-    Context 'Single Values' {
-        It 'Returns the correct value on the node' {
-            $scriptBlock = { Resolve-DscConfigurationProperty -Node $Node -PropertyName 'NodeLevel1\Property' }
-            $scriptBlock | Should Not Throw
-            & $scriptBlock | Should Be 'Node Value'
-        }
-
-        It 'Returns the correct value on the service' {
-            $scriptBlock = { Resolve-DscConfigurationProperty -Node $Node -PropertyName 'ServiceLevel1\Property' }
-            $scriptBlock | Should Not Throw
-            & $scriptBlock | Should Be 'Service Value'
-        }
-
-        It 'Returns the correct value on the site' {
-            $scriptBlock = { Resolve-DscConfigurationProperty -Node $Node -PropertyName 'SiteLevel1\Property' }
-            $scriptBlock | Should Not Throw
-            & $scriptBlock | Should Be 'Site Value'
-        }
-
-        It 'Returns the correct global value' {
-            $scriptBlock = { Resolve-DscConfigurationProperty -Node $Node -PropertyName 'GlobalLevel1\Property'}
-            $scriptBlock | Should Not Throw
-            & $scriptBlock | Should Be 'Global Value'
-        }
-    }
-
     Context 'Multiple Values' {
         $ConfigurationData['Services']['NewService'] = @{
             Nodes = @(
@@ -132,7 +275,7 @@ Describe 'Resolve-DscConfigurationProperty' {
             $scriptBlock | Should Not Throw
 
             $result = (& $scriptBlock) -join ', '
-            $result | Should Be 'Node Value, Site Value, Service Value, Service Value, Global Value'
+            $result | Should Be 'Service Value, Service Value, Node Value, Site Value, Global Value'
         }
     }
 }
