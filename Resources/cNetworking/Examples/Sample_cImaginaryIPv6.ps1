fc773720bc21966c8cd7d7c59e0410a3ad9e663c
snobu <foo@snobu.org>
2014-09-15 17:14:46 +0300

    cNetworking 2.1 Release. Includes cImaginaryIPv6 Resource.

---
+++ Resources/cNetworking/Examples/Sample_cImaginaryIPv6.ps1
@@ -0,0 +1,40 @@
+﻿# Sample configuration for cImaginaryIPv6 DSC Resource
+# Description: Enable/Disable IPv6 Transition Mechanism: 6to4, Teredo, ISATAP
+# Feedback/jinx to: foo@snobu.org / @evilSnobu
+# Windows 8+ / Windows Server 2012+ only
+
+Configuration Sample_ImaginaryIPv6
+{
+    param
+    (
+        [string[]]$NodeName = 'localhost'
+        <#
+        [Parameter(Mandatory=$True)]
+        [ValidateNotNullOrEmpty()]
+        [ValidateSet("Default","Enabled","Disabled")]
+        [String]$SixToFour,
+        
+        [Parameter(Mandatory=$True)]
+        [ValidateNotNullOrEmpty()]
+        [ValidateSet("Automatic","Client","Default","Disabled","Enterpriseclient","Server")]
+        [String]$Teredo,
+
+        [Parameter(Mandatory=$True)]
+        [ValidateNotNullOrEmpty()]
+        [ValidateSet("Default","Enabled","Disabled")]
+        [String]$ISATAP
+        #>
+	)
+
+    Import-DscResource -Module cNetworking
+
+    Node $NodeName
+    {
+        cImaginaryIPv6 Imaginationland
+        {
+           SixToFour = "Disabled"
+              Teredo = "Disabled"
+              ISATAP = "Disabled"
+        }
+    } #Node
+}
