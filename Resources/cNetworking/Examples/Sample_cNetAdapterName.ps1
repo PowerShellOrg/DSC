c287f2014dc8b58e4497739c8cd4194cc4830b77
Daniel Krebs <askdan1el42@gmail.com>
2014-10-03 11:55:49 +0200

    New community resource PSHOrg_cNetAdapterName for Windows Server 2008 R2 and later
    - New DSC resource cNetAdapterName (PSHOrg_cNetAdapterName) using
    CIM/WMI class Win32_NetworkAdapter
    - Added resource root module with help function Convert-MacAddress
    - Modified Author, CompanyName and Copyright to "PowerShell.org
    Community" in cNetworking.psd1

---
+++ Resources/cNetworking/Examples/Sample_cNetAdapterName.ps1
@@ -0,0 +1,21 @@
+﻿Configuration NetAdapterRename
+{
+    Param
+    (
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [System.String]$NewName,
+
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [System.String]$MacAddress
+    )
+
+    Import-DscResource -ModuleName cNetworking
+
+    cNetAdapterName $NewName
+    {
+        NewName = $NewName
+        MacAddress = $MacAddress
+    }
+}
