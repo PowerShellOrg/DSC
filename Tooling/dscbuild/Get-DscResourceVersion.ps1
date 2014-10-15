500bc6683f5d7b8f1942b24c4f7573d12a0ab0d0
Dave Wyatt <dlwyatt115@gmail.com>
2014-10-15 00:31:44 -0400

    Removed calls to Invoke-Expression when importing PSD1 files in the DscBuild module.
    As reported, this was causing some goofy bugs when signed PSD1 files are involved ("executable code in signature block" errors, mainly.)

--- Tooling/dscbuild/Get-DscResourceVersion.ps1
+++ Tooling/dscbuild/Get-DscResourceVersion.ps1
@@ -15,8 +15,8 @@ function Get-ModuleVersion
     Write-Verbose "Checking for $ModulePSD1"
     if (Test-Path $ModulePSD1)
     {
-        $psd1 = get-content $ModulePSD1 -Raw
-        $Version = (Invoke-Expression -Command $psd1)['ModuleVersion']
+        $hashtable = Import-DataFile -Path $ModulePSD1
+        $Version = $hashtable['ModuleVersion']
         Write-Verbose "Found version $Version for $ModuleName."
         Write-Verbose ''
         if ($asVersion) {
@@ -46,8 +46,8 @@ function Get-ModuleAuthor
 
     if (Test-Path $ModulePSD1)
     {
-        $psd1 = get-content $ModulePSD1 -Raw
-        $Author = (Invoke-Expression -Command $psd1)['Author']
+        $hashtable = Import-DataFile -Path $ModulePSD1
+        $Author = $hashtable['Author']
         Write-Verbose "Found author $Author for $ModuleName."
         return $Author
     }
@@ -91,3 +91,30 @@ function Test-ModuleVersion {
 
 }
 
+function Import-DataFile
+{
+    param (
+        [Parameter(Mandatory)]
+        [string] $Path
+    )
+
+    try
+    {
+        $content = Get-Content -Path $path -Raw -ErrorAction Stop
+        $scriptBlock = [scriptblock]::Create($content)
+
+        [string[]] $allowedCommands = @(
+            'Import-LocalizedData', 'ConvertFrom-StringData', 'Write-Host', 'Out-Host', 'Join-Path'
+        )
+
+        [string[]] $allowedVariables = @('PSScriptRoot')
+
+        $scriptBlock.CheckRestrictedLanguage($allowedCommands, $allowedVariables, $true)
+
+        return & $scriptBlock
+    }
+    catch
+    {
+        throw
+    }
+}
