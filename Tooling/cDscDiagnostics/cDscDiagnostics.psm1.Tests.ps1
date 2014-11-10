485adc822833c72e81e9b8bd09cb84ecda76a5ec
Tyson J. Hayes <artwdrahn@gmail.com>
2014-11-10 09:11:57 -0800

    Describing Trace-DscOperationInternal

--- Tooling/cDscDiagnostics/cDscDiagnostics.psm1.Tests.ps1
+++ Tooling/cDscDiagnostics/cDscDiagnostics.psm1.Tests.ps1
@@ -67,6 +67,127 @@ InModuleScope cDscDiagnostics {
             $ErrorAction = $ePreference;
         }
     }
+
+    Describe 'Trace-DscOperationInternal' {
+        Context 'SequenceID is passed' {
+            Mock -ModuleName cDscDiagnostics Log { }
+            $result = Trace-DscOperationInternal -SequenceID 0;
+            
+            It 'should return null if SequenceID is less then 1' {
+                $result | Should Be $null;
+            }
+
+            $date = Get-Date;
+            $message = "Some Message";
+            # Choosing Application because we need /something/ here and we can't assume that the machine has run a DSC command.
+            $event = Get-WinEvent -LogName Application -MaxEvents 1
+
+            $traceOutput = New-Object PSObject -Property @{
+                Type = "Operational";
+                TimeCreated = $date;
+                Message = $message;
+                ComputerName = $env:ComputerName;
+                SequenceID = 2;
+                Event = $event;
+            }
+
+            Mock -ModuleName cDscDiagnostics Get-SingleDscOperation { return @{
+                    AllEvents = $traceOutput;
+                    ErrorEvents = "SomeEvent";
+                    JobId = "54137c5a-f607-48a5-9311-d6e102c15f83";
+                }
+            }
+
+            Mock -ModuleName cDscDiagnostics Test-DscEventLogStatus { }
+            Mock -ModuleName cDscDiagnostics Get-DscErrorMessage { return "Error Text" }
+
+            $result = Trace-DscOperationInternal -SequenceID 2;
+
+            It 'should do call Test-DscEventLogStatus' {
+                Assert-MockCalled Test-DscEventLogStatus -ModuleName cDscDiagnostics -Times 2
+            }
+
+            It 'should call Get-SingleDscOperation' {
+                Assert-MockCalled Get-SingleDscOperation -ModuleName cDscDiagnostics -Times 1
+            }
+
+            It 'should call Get-DscErrorMessage' {
+                Assert-MockCalled Get-DscErrorMessage -ModuleName cDscDiagnostics -Times 1
+            }
+
+            It 'should return the correct event type' {
+                $result.EventType | Should Be "Operational";
+            }
+
+            It 'should return the correct time' {
+                $result.TimeCreated | Should Be $date;
+            }
+
+            It 'should return the correct message' {
+                $result.Message | Should Be $message;
+            }
+
+            It 'should return the correct machine' {
+                $result.ComputerName | Should Be $env:ComputerName;
+            }
+
+            It 'should return the correct SequenceID' {
+                $result.SequenceID | Should Be 2;
+            }
+
+            It 'should return the correct Event' {
+                $result.Event | Should Be $event;
+            }
+        }
+
+        Context 'JobId Passed' {
+            $date = Get-Date;
+            $message = "Some Message";
+            # Choosing Application because we need /something/ here and we can't assume that the machine has run a DSC command.
+            $event = Get-WinEvent -LogName Application -MaxEvents 1
+
+            $traceOutput = New-Object PSObject -Property @{
+                Type = "Operational";
+                TimeCreated = $date;
+                Message = $message;
+                ComputerName = $env:ComputerName;
+                SequenceID = 1;
+                Event = $event;
+            }
+
+            Mock -ModuleName cDscDiagnostics Get-SingleDscOperation { return @{
+                    AllEvents = $traceOutput;
+                    ErrorEvents = "SomeEvent";
+                    JobId = "54137c5a-f607-48a5-9311-d6e102c15f83";
+                }
+            }
+
+            Mock -ModuleName cDscDiagnostics Test-DscEventLogStatus { }
+            Mock -ModuleName cDscDiagnostics Get-DscErrorMessage { return "Error Text" }
+
+            $result = Trace-DscOperationInternal -SequenceID 1 -JobId "54137c5a-f607-48a5-9311-d6e102c15f83";
+
+            It 'should return the Job Id' {
+                $result.JobId | Should Be "54137c5a-f607-48a5-9311-d6e102c15f83"
+            }
+        }
+
+        Context 'Get-SingleDscOperation returns null' {
+            Mock -ModuleName cDscDiagnostics Test-DscEventLogStatus { }
+            Mock -ModuleName cDscDiagnostics Get-SingleDscOperation { }
+
+            $result = Trace-DscOperationInternal -SequenceID 1 -JobId "54137c5a-f607-48a5-9311-d6e102c15f83";
+
+            It 'should return null' {
+                $result | Should Be $null;
+            }
+        }
+
+        # This way we don't cache anything while testing.
+        AfterEach {
+            Clear-DscDiagnosticsCache
+        }
+    }
 }
 
 Describe "Get-cDscOperation" {
