97fbeb402accf99de8fd79be5f429a812e3275cd
David Jones <BladeFireLight@outlook.com>
2015-06-17 23:09:20 -0500

    Added option for Wildcard and regex to service nodes list when used with Resolve-DscConfigurationProperty (this is about 20 times slower) for each Node under $ConfigurationData.services."servicename".nodes are processed as follows If Node contain \.$^+?{}[]  use -Match elseif Node contain *  use -like else  use -eq

--- Tooling/DscConfiguration/Resolve-ConfigurationProperty.ps1
+++ Tooling/DscConfiguration/Resolve-ConfigurationProperty.ps1
@@ -164,6 +164,66 @@ function Get-ServiceValue
     }
 }
 
+function Find-NodeInService
+{
+    [CmdletBinding()]
+    [OutputType([bool])]
+    Param
+    (
+        # ConfigurationData Node Hashtable     
+        [hashtable]
+        $Node,
+
+        # ConfigurationData Service Node Array
+        [String[]]
+        $ServiceNodes
+    )
+    foreach ($serviceNode in $ServiceNodes) 
+    {
+        
+        if ($serviceNode.Contains('\') -or
+            $serviceNode.Contains('.') -or
+            $serviceNode.Contains('$') -or
+            $serviceNode.Contains('^') -or
+            $serviceNode.Contains('+') -or
+            $serviceNode.Contains('?') -or
+            $serviceNode.Contains('{') -or
+            $serviceNode.Contains('}') -or
+            $serviceNode.Contains('[') -or
+            $serviceNode.Contains(']')
+            )
+        {
+           Write-Verbose   "Checking if Node [$($node.Name)] -match [$serviceNode]"
+            if ($node.Name -Match $serviceNode)
+            {
+                
+               return $true
+            }
+
+        }
+        elseif ($serviceNode.contains('*'))
+        {
+            
+           Write-Verbose   "Checking if Node [$($node.Name)] -like [$serviceNode]"
+            if ($node.Name -like $serviceNode)
+            {
+               
+               return $true
+            }
+        }
+        else 
+        {
+           Write-Verbose   "Checking if Node [$($node.Name)] -eq [$serviceNode]"
+            if ($node.Name -eq $serviceNode)
+            {
+               return  $true
+            }
+        }
+    }
+    return $false
+}
+
+
 function ShouldProcessService
 {
     param (
@@ -173,7 +233,7 @@ function ShouldProcessService
         [hashtable] $Node
     )
 
-    $isNodeAssociatedWithService = ($Node.Name -and ($Service['Nodes'] -contains $Node.Name)) -or
+    $isNodeAssociatedWithService = ($Node.Name -and (Find-NodeInService -Node $Node.Name -ServiceNodes $Service.Nodes -Verbose)) -or
                                    ($Node['MemberOfServices'] -contains $ServiceName)
 
     if (-not $isNodeAssociatedWithService)
